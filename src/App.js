564f26c68f22c677dd8d4a87f16e72a84d7f646d
Will <will.aramirez87@gmail.com>
2020-07-30 18:08:05 -0400

    Change React presentation

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         El pana Leo viendo como uso netlify con un dominio propio =D
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hablame Leiyo!
         </a>
       </header>
     </div>
