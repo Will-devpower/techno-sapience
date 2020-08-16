5a5192f02747de22863edf607b751a7206f16167
Will <will.aramirez87@gmail.com>
2020-08-15 21:54:22 -0400

    Added Background and header navigation

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,15 +1,51 @@
 import React from 'react';
-import './header.css';
+import './style.css';
+import logo from '../../images/logo.png';
 import { connect } from 'react-redux';
 import { getNumber } from '../../actions/getAction';
 import { clearCart } from '../../actions/clearCart';
+import { Link } from 'react-router-dom';
 
 
 const Header = (props) => { 
   console.log(props, "Desde el header") 
     return (
       <div id="header">
-        
+		  <div className="nav-header">
+		  	<div className="top-header">
+				<Link to="/" className="logo"><h1>Techno Sapience</h1></Link>
+				<ul id="navigation">
+					<li className="selected">
+						<Link to="/">Home</Link>
+					</li>
+					<li>
+						<a href="#">About</a>
+					</li>
+					<li className="menu">
+						<a href="#">Projects</a>
+						<ul className="primary">
+							<li>
+								<a href="#">proj 1</a>
+							</li>
+						</ul>
+					</li>
+					<li className="menu">
+						<a href="#">Blog</a>
+						<ul className="primary">
+							<li>
+								<a href="#">Single post</a>
+							</li>
+						</ul>
+					</li>
+					<li>
+						<a href="#">Contact</a>
+					</li>
+				</ul>
+			</div>
+		  </div>
+		  <div className="nav-body">
+		  
+		  </div>        
 	  </div>
     )
 }
