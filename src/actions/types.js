a1cd70c66ce1bfd418502669cb099721af57b121
Will <will.aramirez87@gmail.com>
2020-08-23 13:17:28 -0400

    New Template added

--- src/actions/types.js
+++
@@ -1,4 +0,0 @@
-export const ADD_TO_CART = "ADD_TO_CART";
-export const GET_COUNTER = "GET_COUNTER";
-export const CLEAR_CART = "CLEAR_CART";
-export const DELETE_ITEM = "DELETE_ITEM";
--- src/reducers/products.js
+++
@@ -1,5 +0,0 @@
-import PRODUCTS from '../data.json';
-const initialState = PRODUCTS;
-export default (state = initialState) => {
-    return state
-}
