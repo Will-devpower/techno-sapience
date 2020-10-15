7a70ce25047f320688b10182c5c5430b95dfdda3
Will <will.aramirez87@gmail.com>
2020-10-15 10:09:51 -0400

    testing navbar responsiveness first time

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -5,13 +5,19 @@ import SignedInLinks from "./SignedInLinks";
 import { signOut } from "../../store/actions/authActions";
 import "./navbar.css";
 
-const Navbar = (props) => {  
+const Navbar = (props) => {
   const { auth, profile } = props;
   const initials = profile.initials;
-  return (    
-      <nav className="navbar navbar-expand-lg my-navbar bg-primary">        
+  return (
+    <div className="row">
+      <div className="col-12">
+        <nav className="navbar navbar-expand-lg my-navbar bg-primary">
           <Link className="my-logo" to="/">
-            <img src="https://firebasestorage.googleapis.com/v0/b/net-react-rdx-fireb.appspot.com/o/ts-logo.png?alt=media&token=cfdb584c-fced-43c4-aaef-b4c92f5d180f" alt="" className="my-logo-img"/>
+            <img
+              src="https://firebasestorage.googleapis.com/v0/b/net-react-rdx-fireb.appspot.com/o/ts-logo.png?alt=media&token=cfdb584c-fced-43c4-aaef-b4c92f5d180f"
+              alt=""
+              className="my-logo-img"
+            />
           </Link>
           <button
             className="navbar-toggler"
@@ -23,31 +29,33 @@ const Navbar = (props) => {
             aria-label="Toggle navigation"
           >
             <i className="fa fa-bars"></i>
-          </button>        
-        <div
-          className="collapse navbar-collapse right-div"
-          id="navbarNavDropdown"
-        >
-          <SignedInLinks />
-                   
-        </div>
-        
-        {
-          auth.uid &&  
-          <div className="admin">
-            <Link to="/war-admin">Admin</Link>
-            <Link to="/war-signin" onClick={props.logOut}>Logout</Link>
-            <p className="initials">{initials}</p>
+          </button>
+          <div
+            className="collapse navbar-collapse right-div"
+            id="navbarNavDropdown"
+          >
+            <SignedInLinks />
           </div>
-        } 
-      </nav>    
+
+          {auth.uid && (
+            <div className="admin">
+              <Link to="/war-admin">Admin</Link>
+              <Link to="/war-signin" onClick={props.logOut}>
+                Logout
+              </Link>
+              <p className="initials">{initials}</p>
+            </div>
+          )}
+        </nav>
+      </div>
+    </div>
   );
 };
 
 const mapStateToProps = (state) => {
   return {
     auth: state.firebase.auth,
-    profile: state.firebase.profile
+    profile: state.firebase.profile,
   };
 };
 
